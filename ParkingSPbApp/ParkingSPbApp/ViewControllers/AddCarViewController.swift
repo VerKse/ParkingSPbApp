e560be390cf834d20ad2796b4b60aae8c238c46e
VerKse <ksenofontovs.cs@gmail.com>
2021-01-18 18:53:58 +0300

    icons, storyboards with view controllers and segues

---
+++ ParkingSPbApp/ParkingSPbApp/ViewControllers/AddCarViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddCarViewController.swift
+//  ParkingSPbApp
+//
+//  Created by Вера Ксенофонтова on 18.01.2021.
+//
+
+import UIKit
+
+class AddCarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
